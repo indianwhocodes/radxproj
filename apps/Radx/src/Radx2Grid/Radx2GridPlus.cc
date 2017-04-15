9468a4ede789e79104c56d2e64f2fb48effd3898
miaoji <admin@hotel-location.net>
2017-04-15 14:17:03 -0400

    Amend a merge issue.

--- apps/Radx/src/Radx2Grid/Radx2GridPlus.cc
+++ apps/Radx/src/Radx2Grid/Radx2GridPlus.cc
@@ -4,68 +4,59 @@
 #include "tbb/task_group.h"
 #include "tbb/tbb.h"
 #include <ctime>
+
 Radx2GridPlus::Radx2GridPlus(std::string pName) { _programName = pName; }
 
 Radx2GridPlus::~Radx2GridPlus() {}
 
 void Radx2GridPlus::processFiles(const vector<string> &filepaths,
                                  const Params &params) {
-	_inputDir = params.input_dir;
-	_outputDir = params.output_dir;
-  
-  
-	std::vector<PolarDataStream> pds;
-	std::vector<Cartesian2Grid> cg;
-	std::vector<Polar2Cartesian> p2c;
+  _inputDir = params.input_dir;
+  _outputDir = params.output_dir;
 
-	for(int i=0; i<filepaths.size(); i++){
-		pds.push_back(PolarDataStream( filepaths[i]));
-		p2c.push_back(Polar2Cartesian(pds[i].getRepository()));
-		cg.push_back(Cartesian2Grid(pds[i].getRepository()));
-	}
+  std::vector<PolarDataStream> pds;
+  std::vector<Cartesian2Grid> cg;
+  std::vector<Polar2Cartesian> p2c;
 
-	clock_t start = clock();
-	for(int i=0; i<filepaths.size(); i++){
-		pds[i].LoadDataFromNetCDFFilesIntoRepository();
-		pds[i].populateOutputValues();
+  for (int i = 0; i < filepaths.size(); i++) {
+    pds.push_back(PolarDataStream(filepaths[i]));
+    p2c.push_back(Polar2Cartesian(pds[i].getRepository()));
+    cg.push_back(Cartesian2Grid(pds[i].getRepository()));
+  }
 
-		 // step 2 : PolarDataStream to Cartesian coordinates
-		 p2c[i].calculateCartesianCoords();
+  clock_t start = clock();
+  for (int i = 0; i < filepaths.size(); i++) {
+    pds[i].LoadDataFromNetCDFFilesIntoRepository();
+    pds[i].populateOutputValues();
 
-		 // step 3 : Gridding
-		 cg[i].calculateGridSize(params);
-		 cg[i].calculateRefGrid();
+    // step 2 : PolarDataStream to Cartesian coordinates
+    p2c[i].calculateCartesianCoords();
 
-	}
-	std::cout<<"Serial time"<<clock() - start << std::endl;
-  
-	int n=filepaths.size();
-	start = clock();
-	tbb::parallel_for(0, n, [&](size_t i){
- 		pds[i].LoadDataFromNetCDFFilesIntoRepository();
-		pds[i].populateOutputValues();
+    // step 3 : Gridding
+    cg[i].calculateGridSize(params);
+    cg[i].calculateRefGrid();
+  }
+  std::cout << "Serial time" << clock() - start << std::endl;
 
-		// step 2 : PolarDataStream to Cartesian coordinates
-		p2c[i].calculateCartesianCoords();
+  int n = filepaths.size();
+  start = clock();
+  tbb::parallel_for(0, n, [&](size_t i) {
+    pds[i].LoadDataFromNetCDFFilesIntoRepository();
+    pds[i].populateOutputValues();
 
-		// step 3 : Gridding
-		cg[i].calculateGridSize(params);
-		cg[i].calculateRefGrid();
+    // step 2 : PolarDataStream to Cartesian coordinates
+    p2c[i].calculateCartesianCoords();
 
-		//std::cout<<filepaths[i]<<" completed!"<<std::endl;
-		});
-  
-	std::cout<<"Parallel time"<<clock() - start << std::endl;
+    // step 3 : Gridding
+    cg[i].calculateGridSize(params);
+    cg[i].calculateRefGrid();
 
+    // std::cout<<filepaths[i]<<" completed!"<<std::endl;
+  });
+
+  std::cout << "Parallel time" << clock() - start << std::endl;
 }
 
 std::string Radx2GridPlus::getInputDir() { throw "Not Implemented"; }
-
-Radx2GridPlus::Radx2GridPlus(std::string pName) { _programName = pName; }
-
-Radx2GridPlus::~Radx2GridPlus() {}
-
-std::string Radx2GridPlus::getInputDir() { throw "Not Implemented"; }
-
 
 std::string Radx2GridPlus::getOutputDir() { throw "Not Implemented"; }
