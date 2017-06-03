1fc34892d4f7d98ee07fad4d7a027ab80a7a6cdb
sugar <sugar1987cn@gmail.com>
2017-06-03 15:36:25 -0400

    Use tbb to locate memory

--- apps/Radx/src/Radx2Grid/Cart2Grid.cpp
+++ apps/Radx/src/Radx2Grid/Cart2Grid.cpp
@@ -1,4 +1,5 @@
 #include "Cart2Grid.hh"
+#include "tbb/blocked_range3d.h"
 #include "tbb/compat/thread"
 #include "tbb/parallel_for.h"
 #include <assert.h>
@@ -29,6 +30,16 @@ resizeArray(std::shared_ptr<vector<vector<vector<bool>>>>& c,
             bool value)
 {
   c->resize(x, vector<vector<bool>>(y, vector<bool>(z, value)));
+}
+
+inline void
+atomicAdd(tbb::atomic<double>& x, double addend)
+{
+  double o, n;
+  do {
+    o = x;
+    n = o + addend;
+  } while (x.compare_and_swap(n, o) != o);
 }
 
 template<typename T>
@@ -84,25 +95,33 @@ Cart2Grid::Cart2Grid(std::shared_ptr<Repository> store, const Params& params)
     const double CellZ = _z_geom.dz * 1000.0;
     const double Z0 = _store->_altitudeAgl;
 
-#pragma omp parallel for collapse(2)
-    for (auto i = 0; i < _DSizeI; i++)
-      for (auto j = 0; j < _DSizeJ; j++)
+    tbb::parallel_for(
+      tbb::blocked_range3d<int>(0, _DSizeI, 0, _DSizeJ, 0, _DSizeK),
+      [=](const tbb::blocked_range3d<int>& r) {
+#ifdef __GNUC__
+#pragma GCC ivdep
+#else
 #pragma ivdep
-        for (auto k = 0; k < _DSizeK; k++) {
-          double posx = DMinX + i * CellX;
-          double posy = DMinY + j * CellY;
-          double posz = DMinZ + k * CellZ;
-          _grid_x->at(i).at(j).at(k) = posx;
-          _grid_y->at(i).at(j).at(k) = posy;
-          _grid_z->at(i).at(j).at(k) = posz;
-          float s = std::sqrt(posx * posx + posy * posy);
-          float el = calculate_elevation(s, posz, Z0);
-          float rg = calculate_range_gate(s, posz, el, Z0);
-          el = el / M_PI * 180.0;
-          _grid_el->at(i).at(j).at(k) = el;
-          _grid_ground->at(i).at(j).at(k) = s;
-          _grid_gate->at(i).at(j).at(k) = rg;
-        }
+#endif
+        for (auto i = r.pages().begin(); i != r.pages().end(); ++i)
+          for (auto j = r.rows().begin(); j != r.rows().end(); ++j)
+            for (auto k = r.cols().begin(); k != r.cols().end(); ++k) {
+              // We're using a Fortran order at here. So be careful.
+              double posx = DMinX + i * CellX;
+              double posy = DMinY + j * CellY;
+              double posz = DMinZ + k * CellZ;
+              _grid_x->at(i).at(j).at(k) = posx;
+              _grid_y->at(i).at(j).at(k) = posy;
+              _grid_z->at(i).at(j).at(k) = posz;
+              float s = std::sqrt(posx * posx + posy * posy);
+              float el = calculate_elevation(s, posz, Z0);
+              float rg = calculate_range_gate(s, posz, el, Z0);
+              el = el / M_PI * 180.0;
+              _grid_el->at(i).at(j).at(k) = el;
+              _grid_ground->at(i).at(j).at(k) = s;
+              _grid_gate->at(i).at(j).at(k) = rg;
+            }
+      });
   }
 
   // Initialize Feild
@@ -124,16 +143,6 @@ Cart2Grid::Cart2Grid(std::shared_ptr<Repository> store, const Params& params)
   if (_params.debug) {
     _timeit("Allocating grids");
   }
-}
-
-inline void
-atomicAdd(tbb::atomic<double>& x, double addend)
-{
-  double o, n;
-  do {
-    o = x;
-    n = o + addend;
-  } while (x.compare_and_swap(n, o) != o);
 }
 
 void
@@ -208,7 +217,11 @@ Cart2Grid::interpGrid()
     // Grab gates
     for (int i = starti; i <= endi; ++i) {
       for (int j = startj; j <= endj; ++j) {
+#ifdef __GNUC__
+#pragma GCC ivdep
+#else
 #pragma ivdep
+#endif
         for (int k = startk; k <= endk; ++k) {
 
           double s, el, rg, posx, posy;
@@ -253,14 +266,10 @@ Cart2Grid::interpGrid()
 
           double w =
             std::pow(0.005, std::pow(alpha, 3.0)) / std::pow(gate_diff, 2.0) +
-            2e-8;
+            1e-8;
 
           for (auto& name : validname) {
             double v = _store->_outFields[name]->at(m);
-            if (v < 0.0) {
-              continue;
-            }
-
             double vw = v * w + 1e-8;
 
             atomicAdd(_outputGridSum[name]->at(i).at(j).at(k), vw);
